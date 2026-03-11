affaf4d20136c1f6d20942df2a628acb78b5cd85
Chaweewan <saweewan2021@gmall.com>
2026-03-11 23:16:13 +0700

    fix prisma seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,3 +5,27 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient({
   log: ['query', 'error', 'warn']
 })
+
+async function main() {
+
+  const password = await bcrypt.hash('123456', 10)
+
+  await prisma.user.create({
+    data: {
+      name: "Admin",
+      email: "admin@test.com",
+      password: password
+    }
+  })
+
+  console.log("Seed data created")
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
