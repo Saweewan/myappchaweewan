4e4c3179ba6068cb441f8c3b54a873e2432f144f
Chaweewan <saweewan2021@gmall.com>
2026-03-11 23:08:47 +0700

    fix prisma seed import

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,15 +5,3 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient({
   log: ['query', 'error', 'warn']
 })
-
-async function main() {
-
-}
-
-main()
-  .catch((e) => {
-    console.error(e)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
